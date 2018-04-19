cc2a1c4c2ee11b290a93508606c88c2ea0d1a0b2
NicolasSR <nicolassilva114@gmail.com>
2018-04-19 19:20:32 -0300

    Subindo pasta docs

--- js/alunos.js
+++ js/alunos.js
@@ -1,153 +1,147 @@
-var alunos = [
-    {
-        'RA': 1701294,
-        'Nome': 'Nicolas Rezende'
-    },
-    {
-        'RA': 1701295,
-        'Nome': 'João da Silva'
-    },
-    {
-        'RA': 1701295,
-        'Nome': 'João da Silva'
-    },
-    {
-        'RA': 1701295,
-        'Nome': 'João da Silva'
-    },
-    {
-        'RA': 1701295,
-        'Nome': 'João da Silva'
-    },
-    {
-        'RA': 1701295,
-        'Nome': 'João da Silva'
-    },
-    {
-        'RA': 1701295,
-        'Nome': 'João da Silva'
-    },
-    {
-        'RA': 1701295,
-        'Nome': 'João da Silva'
-    },
-    {
-        'RA': 1701295,
-        'Nome': 'João da Silva'
-    },
-    {
-        'RA': 1701295,
-        'Nome': 'João da Silva'
-    },
-    {
-        'RA': 1701295,
-        'Nome': 'João da Silva'
-    },
-    {
-        'RA': 1701295,
-        'Nome': 'João da Silva'
-    },
-    {
-        'RA': 1701295,
-        'Nome': 'João da Silva'
-    },
-    {
-        'RA': 1701295,
-        'Nome': 'João da Silva'
-    },
-    {
-        'RA': 1701295,
-        'Nome': 'João da Silva'
-    },
-    {
-        'RA': 1701295,
-        'Nome': 'João da Silva'
-    },
-    {
-        'RA': 1701295,
-        'Nome': 'João da Silva'
-    },
-    {
-        'RA': 1701295,
-        'Nome': 'João da Silva'
-    },
-    {
-        'RA': 1701295,
-        'Nome': 'João da Silva'
-    },
-    {
-        'RA': 1701295,
-        'Nome': 'João da Silva'
-    },
-    {
-        'RA': 1701295,
-        'Nome': 'João da Silva'
-    },
-    {
-        'RA': 1701295,
-        'Nome': 'João da Silva'
+
+document.getElementById("formulario").onsubmit=
+function(event){
+    var cpfAluno = document.getElementById("cpfaluninho").value;
+    var senhaAluno = document.getElementById("senha").value;
+    var confirmarSenha = document.getElementById("confirmSenha").value;
+    var dataNasc  = document.getElementById("nascimentoAluno");
+    convertDate = new Date(dataNasc.value);
+    var ano,mes,dias,dataAtual, diaAtual,mesAtual,anoAtual,resulIdade,desc,resp;
+    ano = convertDate.getFullYear();
+    mes = convertDate.getMonth()+1;
+    dias = convertDate.getDate()+1;
+    
+    dataAtual = new Date();
+    diaAtual = dataAtual.getDate()+1;
+    mesAtual = dataAtual.getMonth()+1;
+    anoAtual= dataAtual.getFullYear();
+
+    resulIdade = anoAtual-ano;
+    if (mesAtual>mes){
+        resulIdade= resulIdade;
+    }else if(mesAtual==mes){
+        if(diaAtual>=dias){
+            resultIdade=resulIdade;
+        }else{
+            resulIdade -=1;
+        }
+
+    }else{
+        resulIdade -=1;
     }
-]
-
-function addDadosTabela(){
-
-    var tabela = document.getElementById('alunos').getElementsByTagName('tbody')[0]
-    var id = 1
-    alunos.forEach(aluno => {
-        aluno.Id = id;
-        var novaLinha = document.createElement('tr');
-        var tdId = document.createElement('td');
-        var tdRa = document.createElement('td');
-        var tdNome = document.createElement('td');
-        var tdSituacao = document.createElement('td');
-
-        tdId.textContent = aluno.Id;
-        tdRa.textContent = aluno.RA
-        tdNome.textContent = aluno.Nome;
-
-        var span1 = document.createElement('span');
-        span1.classList.add('aprovado');
-        var input1 = document.createElement('input');
-        input1.type = "radio";
-        input1.name = 'situacao' + aluno.Id;
-        input1.id = 's' + aluno.Id + 'A';
-
-        var label1 = document.createElement('label');
-        label1.setAttribute('for', 's' + aluno.Id + 'A');
-        label1.textContent = 'Aprovado';
-
-        span1.appendChild(input1);
-        span1.appendChild(label1);
-
-
-        var span2 = document.createElement('span');
-        span2.classList.add('cancelado');
-        var input2 = document.createElement('input');
-        input2.type = "radio";
-        input2.name = 'situacao' + aluno.Id;
-        input2.id = 's' + aluno.Id + 'B';
-
-        var label2 = document.createElement('label');
-        label2.setAttribute('for', 's' + aluno.Id + 'B');
-        label2.textContent = 'Cancelado';
-
-        span1.appendChild(input1);
-        span1.appendChild(label1);
-
-        span2.appendChild(input2);
-        span2.appendChild(label2);
-
-        tdSituacao.appendChild(span1);
-        tdSituacao.appendChild(span2);
-
-        novaLinha.appendChild(tdId);
-        novaLinha.appendChild(tdRa);
-        novaLinha.appendChild(tdNome);
-        novaLinha.appendChild(tdSituacao);
+    if (resulIdade>=17){
+        resp = true;
+    }else{
+        resp=false;
+        desc='Apenas alunos com idade igual ou superior a 17 podem realizar o cadastro';
+        alert(desc);
+        limpaTela();
         
-        tabela.appendChild(novaLinha);
-        id++;
-    });
+    }
+
+
+    if (resp==true){
+        x = (validaCPF(cpfAluno));
+        if(x==true){
+            resp=true;
+            senha = confereSenha(senhaAluno,confirmarSenha);
+            if(senha==true){
+                caracnum = vSenha(senhaAluno);
+
+                if(caracnum==true){
+                    resp=true;
+                }else{
+                    resp=false;
+                    alert("É necessário possuir uma senha com ao menos um caracter e um número:");
+                    limpaTela();
+                    document.getElementById("senha").focus();
+
+                }
+                
+            }else{
+                alert("As senhas não conscidem");
+            
+                senhaAluno = document.getElementById("senha").focus();
+                
+                resp=false;
+            }
+        }else{
+            resp=false;
+            alert("CPF Inválido!")
+            document.getElementById("cpfaluninho").focus();
+        }
+    }
+
+    return resp
+    limpaTela();
 }
 
-addDadosTabela();
+function validaCPF(cpf)
+  {
+    var numeros, digitos, soma, i, resultado, digitos_iguais;
+    digitos_iguais = 1;
+    if (cpf.length < 11)
+          return false;
+    for (i = 0; i < cpf.length - 1; i++)
+          if (cpf.charAt(i) != cpf.charAt(i + 1))
+                {
+                digitos_iguais = 0;
+                break;
+                }
+    if (!digitos_iguais)
+          {
+          numeros = cpf.substring(0,9);
+          digitos = cpf.substring(9);
+          soma = 0;
+          for (i = 10; i > 1; i--)
+                soma += numeros.charAt(10 - i) * i;
+          resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+          if (resultado != digitos.charAt(0))
+                return false;
+          numeros = cpf.substring(0,10);
+          soma = 0;
+          for (i = 11; i > 1; i--)
+                soma += numeros.charAt(11 - i) * i;
+          resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+          if (resultado != digitos.charAt(1))
+                return false;
+          
+          return true;
+
+          }
+    else
+        return false;
+  }
+  function confereSenha(senha1,senha2){
+        if(senha1==senha2){
+            return true;
+        }else{
+            return false;
+        }
+  }
+
+
+function limpaTela(){
+    document.getElementById("senha").value="";
+    document.getElementById("confirmSenha").value="";
+    document.getElementById("loginAlunos").value="";
+    document.getElementById("nomeAluno").value="";
+    document.getElementById("email").value="";
+    document.getElementById("celular").value="";
+    document.getElementById("nascimentoAluno").value="";
+    document.getElementById("cpfaluninho").value="";
+}
+function vSenha(senhaAluno){
+    numLetras = 0;
+    num = 0;
+    var char = senhaAluno.split('');
+    for(let car of char){
+        if (car.match(/\d/)) num++;
+        else if (car.match(/\w/)) numLetras++;
+    }
+    if((num>=1)&&(numLetras>=1)){
+        return true;
+    }else{
+        return false;
+    }
+}
