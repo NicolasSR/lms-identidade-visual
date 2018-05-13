a6d48c1a9d342bccf8439e80987a3c960c3856a5
NicolasSR <nicolassilva114@gmail.com>
2018-05-13 11:39:51 -0300

    CRUD de Coordenador

--- lms/contas/urls.py
+++ lms/contas/urls.py
@@ -4,8 +4,12 @@ from contas import views
 app_name = 'contas'
 
 urlpatterns = [
-    path('', views.listar_alunos, name='lista-alunos'),
+    path('lista-alunos/', views.listar_alunos, name='lista-alunos'),
     path('cadastrar-aluno/', views.cadastrar_alunos, name='cadastrar-aluno'),
     path('altera-aluno/<int:aluno_id>/', views.alterar_aluno, name='altera-aluno'),
-    path('excluir-aluno/<int:aluno_id>/', views.excluir_aluno, name='excluir-aluno')
+    path('excluir-aluno/<int:aluno_id>/', views.excluir_aluno, name='excluir-aluno'),
+    path('lista-coordenadores/', views.listar_coordenadores, name='lista-coordenadores'),
+    path('cadastrar-coordenador/', views.cadastrar_coordenador, name='cadastrar-coordenador'),
+    path('altera-coordenador/<int:coordenador_id>/', views.alterar_coordenador, name='altera-coordenador'),
+    path('excluir-coordenador/<int:coordenador_id>/', views.excluir_coordenador, name='excluir-coordenador')
 ]
--- lms/contas/views.py
+++ lms/contas/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render, redirect
-from contas.models import Aluno
+from contas.models import Aluno, Coordenador
 
 def cadastrar_alunos(request):
     if request.method == 'GET':
         return render(request, 'cadastro-aluno.html')
     else:
-        id = 0
         if request.POST.get('id'):
-            id = request.POST.get('id')
-        aluno = Aluno(id=id, nome=request.POST.get('nome'), login=request.POST.get('login'), senha=request.POST.get('senha'),
-                      email=request.POST.get('email'), celular=request.POST.get('celular'),
-                      data_nascimento=request.POST.get('nascimento'),
-                      cpf=request.POST.get('cpf'), ra=request.POST.get('ra'))
-        aluno.save()
-        return redirect('/contas')
+            aluno = Aluno(id=request.POST.get('id'), nome=request.POST.get('nome'), login=request.POST.get('login'),
+                          senha=request.POST.get('senha'),
+                          email=request.POST.get('email'), celular=request.POST.get('celular'),
+                          data_nascimento=request.POST.get('nascimento'),
+                          cpf=request.POST.get('cpf'), ra=request.POST.get('ra'))
+            aluno.save()
+        else:
+            aluno = Aluno(nome=request.POST.get('nome'), login=request.POST.get('login'),
+                          senha=request.POST.get('senha'),
+                          email=request.POST.get('email'), celular=request.POST.get('celular'),
+                          data_nascimento=request.POST.get('nascimento'),
+                          cpf=request.POST.get('cpf'), ra=request.POST.get('ra'))
+            aluno.save()
+        return redirect('/contas/lista-alunos')
 
 def listar_alunos(request):
     return render(request, 'lista-alunos.html', {'alunos': Aluno.objects.all()})
@@ -27,4 +33,31 @@ def alterar_aluno(request, aluno_id):
 def excluir_aluno(request, aluno_id):
     aluno = Aluno.objects.get(id=aluno_id)
     aluno.delete()
-    return redirect('/contas')
+    return redirect('/contas/lista-alunos')
+
+def cadastrar_coordenador(request):
+    if request.method == 'GET':
+        return render(request, 'cadastro-coordenador.html')
+    else:
+        if request.POST.get('id'):
+            print('aqui')
+            coordenador = Coordenador(id=request.POST.get('id'), login=request.POST.get('login'), senha=request.POST.get('senha'), nome=request.POST.get('nome'),
+                                  email=request.POST.get('email'), celular=request.POST.get('celular'))
+            coordenador.save()
+        else:
+            coordenador = Coordenador(login=request.POST.get('login'), senha=request.POST.get('senha'), nome=request.POST.get('nome'),
+                                  email=request.POST.get('email'), celular=request.POST.get('celular'))
+            coordenador.save()
+        return redirect('/contas/lista-coordenadores')
+
+def listar_coordenadores(request):
+    return render(request, 'lista-coordenadores.html', {'coordenadores': Coordenador.objects.all()})
+
+def alterar_coordenador(request, coordenador_id):
+    coordenador = Coordenador.objects.get(id=coordenador_id)
+    return render(request, 'cadastro-coordenador.html', {'coordenador': coordenador})
+
+def excluir_coordenador(request, coordenador_id):
+    coordenador = Coordenador.objects.get(id=coordenador_id)
+    coordenador.delete()
+    return redirect('/contas/lista-coordenadores')
